7f47f97b184103444082a09dbdb514f950d3d828
Yuan Yao <andrewyaoinzz@gmail.com>
2019-11-06 14:48:14 -0500

    .

--- readtools/read.cpp
+++ readtools/read.cpp
@@ -7,16 +7,27 @@ int main(int argc, char *argv[]) {
 
   if (argc == 2) {
     std::ifstream values_file(argv[1], std::ifstream::binary);
+    std::cout<<"reading..."<<std::endl;
     auto values = hps::from_stream<std::vector<double>>(values_file);
 
     std::cout<<"No. nonzero entries: "<<values.size()<<std::endl;
-    double normalization = 0., max_elem = 0.;
+    double normalization = 0., max_elem = 0., min_elem = 2.;
+    size_t count1 = 0;
+    double normalization1 =0.;
     for (const double value: values) {
-      if (std::abs(value) > max_elem) max_elem = value;
+      double abs_value = std::abs(value);
+      if (abs_value > max_elem) max_elem = abs_value;
+      else if (abs_value < min_elem) min_elem = abs_value;
       normalization += value * value;
+      if (abs_value > 1e-4) {
+        count1++;
+        normalization1 += value * value;
+      }
     }
     std::cout<<"Max magnitude element: "<<max_elem<<std::endl;
+    std::cout<<"Min magnitude element: "<<min_elem<<std::endl;
     std::cout<<"Normalization: "<<normalization<<std::endl;
+    std::cout<<"Above 1e-4: No. entries: "<<count1<<" normalization: "<<normalization1<<std::endl;
     std::cout<<"\n";
 
   } else {
